1653aaa8a2428f3a316278ebb4b9d4025ae35b93
Eklas Mohin <mohin73k@gmail.com>
2025-05-30 16:28:14 +0600

    Codeforces 4A - Watermelon

--- A/watermelon.cpp
+++ A/watermelon.cpp
@@ -8,7 +8,7 @@ int main() {
     int w{};
     std::cin >> w;
 
-    if (w > 2 && w % 2 == 0) {
+    if (w % 2 == 0 && w > 2) {
         std::cout << "YES\n";
     } else {
         std::cout << "NO\n";
