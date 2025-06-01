9fa0ef6bd8cf514563fb47681b66715c5fbcfbf5
Eklas Mohin <mohin73k@gmail.com>
2025-06-02 01:35:06 +0600

    Codeforces 1985B - Maximum Multiple Sum

---
+++ B/maximum_multiple_sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t{};
+    std::cin >> t;
+
+    while (t--) {
+        int n{};
+        std::cin >> n;
+        if (n == 3) {
+            std::cout << 3 << '\n';
+        } else {
+            std::cout << 2 << '\n';
+        }
+    }
+
+    return 0;
+}
